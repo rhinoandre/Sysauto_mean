7e1e33381440ef5a09c43e890bcf89aa7f0f4233
André Bazoli <andre.bazoli@inatel.br>
2015-12-03 16:55:08 -0200

    Server running
    I created index.html and server.js. Now if you run node server.js from
    this page you'll be able to run this simple example (for now)

--- config/express.js
+++ config/express.js
@@ -1,1 +1,13 @@
-var app = require('express')();
+var express = require('express');
+
+module.exports = function(){
+    var app = express();
+
+    //enviroment config
+    app.set('port', 3000);
+
+    //configuring express to use public folder
+    app.use(express.static('./public'));
+
+    return app;
+};
